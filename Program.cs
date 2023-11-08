b67f842941dbb623953f6f65437042e8a27a82d4
Adr1anL <146196415+Adr1anL@users.noreply.github.com>
2023-11-08 09:07:34 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pppzv6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesi string: ");
+
+            string a = Console.ReadLine();
+
+            if (a.Length < 5) Console.WriteLine(a);
+
+            else
+            {
+                Console.WriteLine(a.Substring(2, a.Length - 4));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
